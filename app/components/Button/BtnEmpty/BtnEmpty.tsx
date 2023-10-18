449d5a0109c2e9f62816334774568ab4a41371eb
KsanaSal <oasalivon@gmail.com>
2023-10-18 22:30:43 +0300

    added button empty

---
+++ app/components/Button/BtnEmpty/BtnEmpty.tsx
@@ -0,0 +1,9 @@
+const BtnEmpty = () => {
+    return (
+        <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+            Button
+        </button>
+    );
+};
+
+export default BtnEmpty;
