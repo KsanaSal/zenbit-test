30668f4d7e5cdaacae82a5f5631465d4442b60e7
KsanaSal <oasalivon@gmail.com>
2023-10-19 21:38:18 +0300

    added images in details page

---
+++ app/details/page.styled.ts
@@ -0,0 +1,6 @@
+import { styled } from "styled-components";
+
+export const SectionDetails = styled.section`
+    background-color: #f2f2f2;
+    padding: 120px 24px;
+`;
---
+++ app/details/page.tsx
@@ -0,0 +1,26 @@
+"use client";
+import { SectionDetails } from "./page.styled";
+
+export default function Details() {
+    return (
+        <SectionDetails>
+            <div>
+                <div>
+                    <img src="" alt="" />
+                    <div>
+                        <h2>The Marina Torch</h2>
+                        <ul>
+                            <li>6 500 000 Dhs</li>
+                            <li>Yield 9.25%</li>
+                            <li>Sold 75%</li>
+                        </ul>
+                        <ul>
+                            <li>Tiket - 60 000 Dhs</li>
+                            <li>Days left 150 </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </SectionDetails>
+    );
+}
