dc8582bff83e9961beb52db27216b00b6ff8ad92
KsanaSal <oasalivon@gmail.com>
2023-10-19 10:47:48 +0300

    added adaptive styles

--- app/components/Button/BtnFull/BtnFull.styled.ts
+++ app/components/Button/BtnFull/BtnFull.styled.ts
@@ -7,11 +7,16 @@ export const Button = styled.button`
     border-radius: 5px;
     padding: 10px 20px;
     text-align: center;
-    min-width: 160px;
+    min-width: 120px;
 
     &:hover {
         background-color: transparent;
         color: #b29f7e;
         transition: 250ms cubic-bezier(0.4, 0, 0.2, 1);
+    }
+
+    @media screen and (min-width: 768px) {
+        min-width: 160px;
+        font-size: 16px;
     }
 `;
--- app/components/Header.styled.ts
+++ app/components/Header.styled.ts
@@ -2,8 +2,17 @@ import { styled } from "styled-components";
 
 export const HeaderStyled = styled.div`
     background-color: #172234;
-    padding: 20px 80px;
+    padding: 20px 24px;
     display: flex;
-    justify-content: flex-end;
-    gap: 10px;
+    justify-content: space-between;
+
+    @media screen and (min-width: 768px) {
+        justify-content: flex-end;
+        padding: 20px 40px;
+        gap: 10px;
+    }
+
+    @media screen and (min-width: 1024px) {
+        padding: 20px 80px;
+    }
 `;
